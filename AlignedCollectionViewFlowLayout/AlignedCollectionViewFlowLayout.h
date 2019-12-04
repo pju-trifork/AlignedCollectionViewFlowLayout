9954d5674e457b8a1c878bf4c6193e6388c7a4bd
Povilas Juzeliunas <pju@trifork.com>
2019-12-04 15:37:29 +0100

    Xcode project file for framework

---
+++ AlignedCollectionViewFlowLayout/AlignedCollectionViewFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  AlignedCollectionViewFlowLayout.h
+//  AlignedCollectionViewFlowLayout
+//
+//  Created by Povilas Juzeliunas on 04/12/2019.
+//  Copyright © 2019 Povilas Juzeliunas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlignedCollectionViewFlowLayout.
+FOUNDATION_EXPORT double AlignedCollectionViewFlowLayoutVersionNumber;
+
+//! Project version string for AlignedCollectionViewFlowLayout.
+FOUNDATION_EXPORT const unsigned char AlignedCollectionViewFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlignedCollectionViewFlowLayout/PublicHeader.h>
+
+
